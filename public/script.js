2b7201a253dbe808f740e37869efb02619c703e3
Xingye972 <chengye171@gmail.com>
2025-04-09 15:18:43 +1200

    chinese

---
+++ public/script.js
@@ -0,0 +1,15 @@
+import { Game } from './game.js';
+
+console.log("script.js is being executed");
+
+let game;
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM content loaded");
+    const gameContainer = document.getElementById("game-container");
+    game = new Game(gameContainer);
+    game.updateGame(); // 开始游戏循环
+
+    // 确保在页面加载时隐藏视频框
+    game.hidePoseCanvas();
+});
